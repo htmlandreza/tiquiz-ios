d5c20275ab45c0373df884bdc72858fb91aff23d
Andreza Moreira <htmlandreza@gmail.com>
2019-04-09 23:58:42 -0300

    Modificando Classe QuizQuestion

--- TIQuiz/TIQuiz/Models/QuizClasses.swift
+++ TIQuiz/TIQuiz/Models/QuizClasses.swift
@@ -10,24 +10,41 @@ import Foundation
 
 // MARK: Questões do Quiz
 // API
-struct QuizQuestion: Codable {
-    let id, topic, question, correctAnswer: String
-    let optionsAnswer: [OptionsAnswer]
-    let createBy: [CreateBy]
-    let createAt, status: String
+//struct QuizQuestion: Codable {
+//    let id, topic, question, correctAnswer: String
+//    let optionsAnswer: [OptionsAnswer]
+//    let createBy: [CreateBy]
+//    let createAt, status: String
+//}
+//
+//struct OptionsAnswer: Codable {
+//    let optionsID: Int
+//    let answer: String
+//}
+//
+//struct CreateBy: Codable {
+//    let institution, teacherName, teacherEmail: String
+//}
+
+struct QuizQuestion {
+let id, question, topic, correctAnswer: String
+let optionsAnswer: [OptionsAnswer]
+let creatBy: [CreatBy]
+let status, createdAt, updatedAt: String
+let v: Int
 }
 
-struct OptionsAnswer: Codable {
+struct CreatBy {
+    let id, intitution, teacherName, teacherEmail: String
+}
+
+struct OptionsAnswer {
     let optionsID: Int
-    let answer: String
-}
-
-struct CreateBy: Codable {
-    let institution, teacherName, teacherEmail: String
+    let id, answer: String
 }
 
 // MARK: Tópicos
-// API
+// JSON
 struct QuizTopic: Codable {
     let id, topic: String
 }
